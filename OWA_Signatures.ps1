93aeca15fd5440328f9b45004a6a5f1d6aac26d9
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2023-01-17 13:34:36 -0300

    First code upload

---
+++ OWA_Signatures.ps1
@@ -0,0 +1,55 @@
+#Install required modules (uncomment if needed)
+#Install-Module -Name AzureAD -Scope AllUsers -Force
+#Install-Module -Name ExchangeOnlineManagement -Scope AllUsers -Force
+
+#Load required modules
+Write-Output "Loading required modules"
+Import-Module -Name ExchangeOnlineManagement -Global | Out-Null
+Import-Module -Name AzureAD -UseWindowsPowerShell -Global | Out-Null
+
+#Script enviroment varibles
+$AppID = "PutYourAppID"
+$CertThumb = "PutYourCertThumbPrint"
+$TenantID = "PutYourTenantID"
+$OrganizationDomain = "PutYourPrincipalDomainName.onmicrosoft.com"
+
+#Adjust your Ldap filter query as you wich
+$LDAPFilter = '{$_.AccountEnabled -like "True" -and $_.Mail -like "*cercena.com.br" -and $_.UserType -like "Member" -and $_.UserPrincipalName -like "*cercena.com.br"}'
+
+#Local where is the script and html template file
+$DefaulWorkDir = "c:\Signature"
+
+#Connect to AzureAD and ExchangeOnline services
+Write-Output "Connecting to remote services ExchangeOnline and AzureAD"
+Connect-AzureAD -ApplicationId $AppID -CertificateThumbprint $CertThumb -TenantId $TenantID | Out-Null
+Connect-ExchangeOnline -AppId $AppID -CertificateThumbprint $CertThumb -Organization $OrganizationDomain -ShowBanner:$false | Out-Null
+
+#Gets all users' data from Azure AD and saves it to an array.
+$users = (Get-AzureADUser -All $true | Where-Object $LDAPFilter | Select-Object DisplayName, JobTitle, TelephoneNumber, Mobile, Mail)
+
+#Saves HTML code of a signature from the signature generator to a variable. Change the path to the location of your file.
+$HTMLsig = Get-Content "$DefaulWorkDir\Template_HTML.html" | out-string
+$UserCount = 0
+
+Write-Output "Starting the routine" > $DefaulWorkDir\lastrun.txt
+
+foreach($user in $users){
+	#Replacing AzureAD placeholders with personal user data
+	$HTMLSigX = $HTMLsig.replace('%%DisplayName%%', $user.DisplayName). `
+	replace('%%JobTitle%%', $user.JobTitle). `
+	replace('%%TelephoneNumber%%', $user.TelephoneNumber). `
+	replace('%%Mobile%%', $user.Mobile). `
+	replace('%%Mail%%', $user.Mail)
+	
+	<#Saves the personalized email signature in mailbox settings. It should be available in Outlook on the web right away.
+	The -AutoAddSignature parameter sets the signature as default for new messages and -AutoAddSignatureOnReply does the same for replies.#>
+	Set-MailboxMessageConfiguration $user.Mail -SignatureHTML $HTMLSigX -AutoAddSignature $true -AutoAddSignatureOnReply $true
+	$UserCount++	
+	Write-Output "Setting signature for user $($UserCount): $($user.Mail)" >> $DefaulWorkDir\lastrun.txt
+}
+Write-Output "Number of users affected: $($UserCount)"
+date >> $DefaulWorkDir\lastrun.txt
+Write-Output "Disconnecting from ExchangeOnline"
+Disconnect-ExchangeOnline -Confirm:$false
+Write-Output "Disconnecting from AzureAD"
+Disconnect-AzureAD -Confirm:$false
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # O365owaSignature
 Powershell script to set a OWS email signature with ldap data and html template.
+
+System requisites:
+This script was created to run with any version of powershell, on this implementation i have used powershell 7.3.1
+
+Powershell requisites/modules:
+- AzureAD
+- ExchangeOnlineManagement
+
+How to run:
+
+1st - Create a AppID account, to use transparent and script like authentication.
+
+Microsoft documentation
+https://learn.microsoft.com/en-us/powershell/exchange/app-only-auth-powershell-v2?view=exchange-ps
+
+
+2st - Get the cert thumbprint from 1st step and hardcode this on the script line
+
